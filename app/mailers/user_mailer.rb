e68907e2007a55ae2145e61d51994bb07e681e93
codeByScott <scott@codebyscott.io>
2016-11-24 10:51:57 +0900

    Add account activation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,19 @@
 class UserMailer < ApplicationMailer
-  default from: "do-not-reply@codebyscott.io"
 
   def contact_email(contact)
     @contact = contact
     mail(to: Rails.application.secrets.owner_email, 
-         from: @contact.email, 
+         from: contact.email, 
          subject: "Website Contact")
+  end
+
+  def account_activation(user)
+    @user = user
+    mail to: user.email, subject: "Account activation"
+  end
+
+  def password_reset
+    @greeting = "Hi"
+    mail to: "to@example.org"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  
+  root 'welcome#index'
   get 'sessions/new'
-
   get 'users/new'
-
   get '/about', to: 'static#about', as: 'about'
-
   resources :articles
   resources :projects
   resources :contacts, only: [:new, :create]
@@ -14,7 +11,8 @@ Rails.application.routes.draw do
   post     '/login',   to: 'sessions#create'
   delete   '/logout',  to: 'sessions#destroy'
   resources :users
-  root 'welcome#index'
+  resources :account_activations, only: [:edit]
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
